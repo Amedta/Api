85bc92d94bc2cb1aa84bf1be5efcdac72ec0a66f
amedta <amedta321go@gmail.com>
2024-08-11 22:14:16 +0700

    new1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const port = process.env.PORT || 3000;
 require('dotenv').config()
 
 const app = express();
